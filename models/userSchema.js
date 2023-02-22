f8a7328e3f9765261d123bbf6bf18fbd9f5fcda9
Deoraj Maharaj <101578606+deoraj2001-git@users.noreply.github.com>
2023-02-22 20:17:41 +0530

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,8 +1,12 @@
 const mongoose=require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  age: { type: Number, required: true },
+  dueDate: { type: Date, required: true },
+  appointments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Appointment' }]
+});
+const User = mongoose.model('User', userSchema);
 
-const userSchema=new mongoose.Schema({
-
-})
-
-const User=new mongoose.model("USER",userSchema);
 module.exports=User;
